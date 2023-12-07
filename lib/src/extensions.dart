cfe56643ead4fc53de26eda2330da50f87e10f84
RijazDev <rijaz.rasheed@mobcoder.com>
2023-12-07 14:41:26 +0300

    fix : renamed list extension

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -1,6 +1,6 @@
 part of health;
 
-extension FirstWhereExt<T> on List<T> {
+extension ListExtension<T> on List<T> {
   /// The first element satisfying [test], or `null` if there are none.
   T? firstWhereOrNull(bool Function(T element) test) {
     for (var element in this) {
